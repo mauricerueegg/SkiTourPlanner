e5bde243653016269f3325b9f58cbc37102b1601
Maurice Rueegg <maurice.rueegg@gmail.ch>
2025-04-04 20:56:55 +0200

    updated utils.py for github actions

--- scraper/utils.py
+++ scraper/utils.py
@@ -2,11 +2,17 @@ import random
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def generate_route_urls(num_ids, id_range):
     random_ids = random.sample(range(id_range[0], id_range[1] + 1), num_ids)
     return [f"https://www.skitourenguru.ch/track-view?area=ch&id={i}" for i in random_ids]
 
+
 def setup_driver():
-    options = Options()
-    options.headless = False
-    return webdriver.Chrome(options=options)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # wichtig für GitHub Actions
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    
+    driver = webdriver.Chrome(options=options)
+    return driver
